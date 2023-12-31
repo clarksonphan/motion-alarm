fb5e7165426e125845a7cf90f9e011a7093da774
ndxdxd <102645495+ndxdxd@users.noreply.github.com>
2023-12-30 22:24:54 -0800

    backend stuff

---
+++ react-website-alarm/backend/routes/alarmStatus.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const alarm = require('../models/alarmStat')
+const {getAlarms} = require('../controllers/alarmControllers')
+const router = express.Router()
+
+//get all the alarm status
+router.get('/', (req, res) => {
+    console.log('App Object:', req.app);
+    getAlarms(req, res);
+  });
+
+//get one alarm status
+router.get('/:id', (req,res) => {
+    res.json({mssg: 'get one alarm status'})
+})
+
+
+module.exports = router 
